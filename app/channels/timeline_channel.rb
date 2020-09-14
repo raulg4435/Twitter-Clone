33af771d4b132440e90cad6c8ffb8a8b856a8d8c
Raul <raul.g4435@gmail.com>
2020-09-13 21:37:51 -0500

    Add CableReady to enable several clients having UI update simultanously

---
+++ app/channels/timeline_channel.rb
@@ -0,0 +1,6 @@
+class TimelineChannel < ApplicationCable::Channel
+  def subscribed
+    stream_from "timeline"
+  end
+
+end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  include CableReady::Broadcaster
 
   # GET /posts
   # GET /posts.json
@@ -11,7 +11,13 @@ class PostsController < ApplicationController
   # POST /posts
   # POST /posts.json
   def create
-    @post = Post.create(post_params)
+    post = Post.create(post_params)
+    cable_ready["timeline"].insert_adjacent_html(
+      selector: "#timeline",
+      position: "afterbegin",
+      html: render_to_string(partial: "post", locals: {post:post})
+    )
+    cable_ready.broadcast
     redirect_to posts_path
   end
 
